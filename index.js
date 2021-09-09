01ab68b07d568064ec828ff391a65ea63e832771
Om Kakatkar <omkakatkar@gmail.com>
2021-09-09 22:39:55 +0530

    create enemies

--- index.js
+++ index.js
@@ -43,11 +43,51 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player1 = new Player(x, y, 20, "blue");
 player1.draw();
+
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const radius = 25;
+    const color = "green";
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 const projectile = new Projectile(
   canvas.width / 2,
@@ -59,25 +99,34 @@ const projectile = new Projectile(
     y: 1,
   }
 );
-const projectile2 = new Projectile(
-  canvas.width / 2,
-  canvas.height / 2,
-  5,
-  "red",
-  {
-    x: 1,
-    y: -1,
-  }
-);
 
-const projectiles = [projectile, projectile2];
-// addEventListener("click", (e) => {});
+const projectiles = [];
+addEventListener("click", (e) => {
+  const angle = Math.atan2(
+    e.clientY - canvas.height / 2,
+    e.clientX - canvas.width / 2
+  );
+
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+  );
+});
 
 function animate() {
   requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player1.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
 animate();
+spawnEnemies();
