4901d97ae3edde18882c821e420b863f8e527f23
Om Kakatkar <omkakatkar@gmail.com>
2021-09-10 22:26:04 +0530

    end game and collision added

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ class Enemy {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player1 = new Player(x, y, 20, "blue");
-player1.draw();
+const player = new Player(x, y, 20, "blue");
+player.draw();
 
 const enemies = [];
 
@@ -123,15 +123,45 @@ addEventListener("click", (e) => {
   );
 });
 
+let animationId;
+
 function animate() {
-  requestAnimationFrame(animate);
+  animationId = requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  player1.draw();
-  projectiles.forEach((projectile) => {
+  player.draw();
+  projectiles.forEach((projectile, projectileIndex) => {
     projectile.update();
+
+    // remove from edges of screen
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(projectileIndex, 1);
+      }, 0);
+    }
   });
-  enemies.forEach((enemy) => {
+  enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+    if (dist - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId);
+    }
+    projectiles.forEach((projectile, projectileIndex) => {
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+
+      if (dist - enemy.radius - projectile.radius < 1) {
+        setTimeout(() => {
+          enemies.splice(enemyIndex, 1);
+          projectiles.splice(projectileIndex, 1);
+        }, 0);
+      }
+    });
   });
 }
 
