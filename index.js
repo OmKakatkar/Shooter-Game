a76cbefa5b92072a872f54ab04e36d2f8d406858
Om Kakatkar <omkakatkar@gmail.com>
2021-09-12 19:52:30 +0530

    fix: white screen display

--- index.js
+++ index.js
@@ -142,17 +142,25 @@ const projectile = new Projectile(
   }
 );
 
-addEventListener("click", (e) => {
+addEventListener("touchStart", (e) => {
   const angle = Math.atan2(
     e.clientY - canvas.height / 2,
     e.clientX - canvas.width / 2
   );
 
-  addEventListener("touchStart", (e) => {
-    const angle = Math.atan2(
-      e.clientY - canvas.height / 2,
-      e.clientX - canvas.width / 2
-    );  
+  const velocity = {
+    x: Math.cos(angle) * 4,
+    y: Math.sin(angle) * 4,
+  };
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "white", velocity)
+  );
+});
+addEventListener("click", (e) => {
+  const angle = Math.atan2(
+    e.clientY - canvas.height / 2,
+    e.clientX - canvas.width / 2
+  );
 
   const velocity = {
     x: Math.cos(angle) * 4,
